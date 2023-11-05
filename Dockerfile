50a907817b3ee539865e9e4a08c2e5dd18f33918
MoetazHajji <moetaz.hajji@esprit.tn>
2023-11-06 06:40:33 +1300

    adding cors configuration

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:11
 EXPOSE 8089
-ADD /target/gestion-station-ski-1.0.jar gestion-station-ski.jar
+ADD /target/gestion-station-ski-1.1.jar gestion-station-ski.jar
 
 ENTRYPOINT ["java", "-jar","gestion-station-ski.jar"]
---
+++ src/main/java/tn/esprit/spring/configs/CorsConfig.java
@@ -0,0 +1,27 @@
+package tn.esprit.spring.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig{
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:4200");
+        configuration.setAllowCredentials(false);
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        configuration.setAllowedHeaders(Arrays.asList("origin", "content-type", "accept", "authorization", "cookie", "refresh-token"));
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
